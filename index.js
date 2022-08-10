3b83ff1efba799e6c779024f064f7ed671a84ac6
unknown <islam15-3879@diu.edu.bd>
2022-08-10 18:18:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000 ;
+
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krrcpvz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// suro
+ async function run(){
+
+    try{
+        await client.connect();
+        console.log('database connected ')
+        const foodCollection =client.db('food_receipe').collection('foods')
+        
+        app.get('/foods',async(req,res)=>{
+            const query ={} ;
+            const cursor =foodCollection.find(query) ;
+            const foods =await cursor.toArray();
+            res.send(foods)
+
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+// ses
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from food receipe!')
+})
+
+app.listen(port, () => {
+  console.log(`Food receipe app listening on port ${port}`)
+})
